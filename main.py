32e29d3270487cac181c03d2d8f84cd31066cd6e
Linda Pei <linda.pei@gmail.com>
2017-06-11 16:54:32 -0700

    Created jinja template htmls

--- main.py
+++ main.py
@@ -1,44 +1,18 @@
 from flask import Flask, request
 from caesar import rotate_string
+import os
+import jinja2
+
+template_dir = os.path.join(os.path.dirname(__file__),'templates')
+jinja_env = jinja2.Environment(loader = jinja2.FileSystemLoader(template_dir), autoescape=True)
 
 app = Flask(__name__)
 app.config['DEBUG'] = True
 
-form = """
-<!DOTYPE html>
-    <html>
-        <head>
-            <style>
-                form {{
-                    background-color: #eee;
-                    padding: 20px;
-                    margin 0 auto;
-                    width: 540px;
-                    font: 16px 'Arial', sans-serif;
-                    border-radius: 10px;
-                }}
-                textarea {{
-                    marin: 10px 0;
-                    width: 540px;
-                    height: 120px;
-                }}
-            </style>
-        </head>
-    <body>
-        <!--create yoru form here-->
-        <form action="/" method="post">
-            Rotate by:
-            <input type="text" name="rot" value="0"/>
-            <textarea name="text">{0}</textarea>
-            <input type="submit" value="Submit Query" />
-        </form>
-    </body>
-</html>
-"""
-
 @app.route("/")
 def index():
-    content = form.format("")
+    template_form = jinja_env.get_template('form.html')    
+    content = template_form.render(textarea_content = "")
 
     return content
 
@@ -48,8 +22,9 @@ def encrypt():
     rotation = request.form['rot']
     textstr = request.form['text']
     encrypted = rotate_string(textstr, int(rotation))
-
-    return form.format(encrypted)
+    template_form = jinja_env.get_template('form.html')
+    
+    return template_form.render(textarea_content = encrypted)
     #return "<h1>" + encrypted + "</h1>"
 
 
