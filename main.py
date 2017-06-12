3d5ef47551e5e523accba16d6fc4db3a0e1250f2
Linda Pei <linda.pei@gmail.com>
2017-06-11 17:37:43 -0700

    created html and css templates

--- main.py
+++ main.py
@@ -1,18 +1,14 @@
-from flask import Flask, request
+from flask import Flask, request, url_for, render_template
 from caesar import rotate_string
-import os
-import jinja2
 
-template_dir = os.path.join(os.path.dirname(__file__),'templates')
-jinja_env = jinja2.Environment(loader = jinja2.FileSystemLoader(template_dir), autoescape=True)
 
 app = Flask(__name__)
 app.config['DEBUG'] = True
 
+
 @app.route("/")
 def index():
-    template_form = jinja_env.get_template('form.html')    
-    content = template_form.render(textarea_content = "")
+    content = render_template('form.html', textarea_content = "", style_sheet = url_for('static', filename='style.css'))
 
     return content
 
@@ -22,9 +18,8 @@ def encrypt():
     rotation = request.form['rot']
     textstr = request.form['text']
     encrypted = rotate_string(textstr, int(rotation))
-    template_form = jinja_env.get_template('form.html')
     
-    return template_form.render(textarea_content = encrypted)
+    return render_template('form.html', textarea_content = encrypted, style_sheet = url_for('static', filename='style.css'))
     #return "<h1>" + encrypted + "</h1>"
 
 
